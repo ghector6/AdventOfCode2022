507df19c1b5300e589dd7ab29d8a9fff41f819c7
Hector Garcia Rosales <ghectormus@gmail.com>
2022-12-12 09:27:22 -0600

    Adding most_calories method

--- Day1/calories_counter.rb
+++ Day1/calories_counter.rb
@@ -2,6 +2,10 @@ class CaloriesCounter
     def calculate(string)
         string.split("\n").collect{|n| n.to_i}.sum
     end 
+    
+    def most_calories(elves_list)
+        elves_list.split("\n\n").collect{|n| calculate(n)}.max
+    end
 end 
 
 if __FILE__ == $0
