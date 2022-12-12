d043fcb308e6fd642e53d34965e681c2ba368849
Hector Garcia Rosales <ghectormus@gmail.com>
2022-12-12 10:17:32 -0600

    Adding solution with 2 methods

--- Day1/calories_counter.rb
+++ Day1/calories_counter.rb
@@ -6,11 +6,14 @@ class CaloriesCounter
     def most_calories(elves_list)
         elves_list.split("\n\n").collect{|n| calculate(n)}.max
     end
+    def top_calories(num, elves_list)
+        elves_list.split("\n\n").collect{|n| calculate(n)}.max(num).sum
+    end
 end 
 
 if __FILE__ == $0
     elves_list = File.read("input.txt")
     counter = CaloriesCounter.new
-    most_calories = counter.most_calories(elves_list)
+    most_calories = counter.top_calories(3,elves_list)
     p most_calories
 end
