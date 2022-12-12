f89ddc843e024f66eb7a68328f661652135b0046
Hector Garcia Rosales <ghectormus@gmail.com>
2022-12-11 22:16:44 -0600

    Step1.1; Test for three different values

--- Day1/calories_counter_spec.rb
+++ Day1/calories_counter_spec.rb
@@ -7,6 +7,15 @@ describe CaloriesCounter do
             calories = counter.calculate("1000")
             expect(calories).to eq(1000)
         end
-        
+        it "Gets 2k calories" do
+            counter = CaloriesCounter.new
+            calories = counter.calculate("2000")
+            expect(calories).to eq(2000)
+        end
+        it "Gets 3k calories" do
+            counter = CaloriesCounter.new
+            calories = counter.calculate("3000")
+            expect(calories).to eq(3000)
+        end
     end
 end
