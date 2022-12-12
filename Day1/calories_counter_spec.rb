22fa0469d5252763b35b1be5a0ca89dedde072db
Hector Garcia Rosales <ghectormus@gmail.com>
2022-12-12 10:21:47 -0600

    Fixing test to receive params in the right order

--- Day1/calories_counter_spec.rb
+++ Day1/calories_counter_spec.rb
@@ -49,7 +49,7 @@ describe CaloriesCounter do
         
         it "return 45000" do
             counter = CaloriesCounter.new
-            calories = counter.top_calories(3, "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000")
+            calories = counter.most_calories("1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000",3)
             expect(calories).to eq(45000)
         end
     end
