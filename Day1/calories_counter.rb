61160ce45ec97ffcf03fc8f0acd8ba6a3967bc7b
Hector Garcia Rosales <ghectormus@gmail.com>
2022-12-12 10:22:21 -0600

    Refactor: Defining a single method for both parts

--- Day1/calories_counter.rb
+++ Day1/calories_counter.rb
@@ -2,18 +2,15 @@ class CaloriesCounter
     def calculate(string)
         string.split("\n").collect{|n| n.to_i}.sum
     end 
-    
-    def most_calories(elves_list)
-        elves_list.split("\n\n").collect{|n| calculate(n)}.max
-    end
-    def top_calories(num, elves_list)
+        
+    def most_calories(elves_list, num=1)
         elves_list.split("\n\n").collect{|n| calculate(n)}.max(num).sum
-    end
+    end    
 end 
 
 if __FILE__ == $0
     elves_list = File.read("input.txt")
     counter = CaloriesCounter.new
-    most_calories = counter.top_calories(3,elves_list)
+    most_calories = counter.most_calories(elves_list,3)
     p most_calories
 end
