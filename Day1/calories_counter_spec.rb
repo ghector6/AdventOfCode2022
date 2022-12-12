ea354f88fd12cc27678d413580883a677e30186d
Hector Garcia Rosales <ghectormus@gmail.com>
2022-12-12 09:15:19 -0600

    Adding a third context for a new method

--- Day1/calories_counter_spec.rb
+++ Day1/calories_counter_spec.rb
@@ -37,5 +37,13 @@ describe CaloriesCounter do
         end
     end
 
+    context "Return the elf carrying the most calories" do
+        
+        it "return 24000"do
+            counter = CaloriesCounter.new
+            calories = counter.most_calories("1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000")
+        end
+    end
+
     
 end
