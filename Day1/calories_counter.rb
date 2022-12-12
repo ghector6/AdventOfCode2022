4e50e1733bd79ea59c5bcc0e2ede9048eabde486
Hector Garcia Rosales <ghectormus@gmail.com>
2022-12-12 09:01:21 -0600

    Adding solution for second context of the tests

--- Day1/calories_counter.rb
+++ Day1/calories_counter.rb
@@ -1,8 +1,11 @@
 class CaloriesCounter
     def calculate(string)
-        string = string.split('\n')
-        string.collect!{ |n| n.to_i}
-        string.sum 
+        string.split("\n").collect{|n| n.to_i}.sum
     end 
-
 end 
+
+if __FILE__ == $0
+    counter = CaloriesCounter.new
+    calories = counter.calculate("1000\n2000\n3000")
+    p calories
+end
