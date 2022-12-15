0dea7d3a2a4c4a66347f997ffe5a532a552b20cd
Hector Garcia Rosales <ghectormus@gmail.com>
2022-12-15 17:20:13 -0600

    Day 2: Step 1 writting test that cuts the instructions received

---
+++ Day2/chinchanpu_spec.rb
@@ -0,0 +1,14 @@
+require_relative 'chinchanpu'
+
+describe ChinChanPu do 
+	context 'It reads chars at a passed string' do 
+		before do 
+			@instructions = ChinChanPu.new
+		end
+
+		it 'returns A' do
+			result = instructions.cutter("A")
+			expect(result).to eq.("A")
+		end
+	end
+end
