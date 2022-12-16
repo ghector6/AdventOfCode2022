4bddc4ec27dc7ea390c87dd88ba277b7dd3220de
Hector Garcia Rosales <ghectormus@gmail.com>
2022-12-15 20:12:12 -0600

    Structuring the test

--- Day2/chinchanpu_spec.rb
+++ Day2/chinchanpu_spec.rb
@@ -3,21 +3,21 @@ require_relative 'chinchanpu'
 describe ChinChanPu do 
 	context 'Returns the splitted elements of and instructions' do 
 		it 'Returns: A' do 
-			instruction_set = ChinChanPu.new
-			instruction_element = instruction_set.cutter("A")
+			instruction_set = ChinChanPu.new(("A"))
+			instruction_element = instruction_set.cutter
 			expect(instruction_element).to eq(["A"])
 		end
 		it 'Returns A Y as single elements' do 
-			instruction_set = ChinChanPu.new
-			instruction_element = instruction_set.cutter("A Y")
+			instruction_set = ChinChanPu.new("A Y")
+			instruction_element = instruction_set.cutter
 			expect(instruction_element).to eq(["A","Y"])
 		end
 		
 	end
 	context 'Given a set of instructions, sums the points of the figure used' do 
 		it 'returns 1 if my figure used is rock (X)' do 
-			instruction_set = ChinChanPu.new
-			instruction_element = instruction_set.figure?(["A","X"])
+			instruction_set = ChinChanPu.new("A X")
+			instruction_element = instruction_set.figure?
 			expect(instruction_element).to eq(1)
 		end
 		#it 'returns 2 if my figure used is paper ()'
