c521c827aaff8989dce489768ba6375a2f1bba6b
Hector Garcia Rosales <ghectormus@gmail.com>
2022-12-15 19:21:17 -0600

    Day2: Writting a test that throws the value of the used figure

--- Day2/chinchanpu_spec.rb
+++ Day2/chinchanpu_spec.rb
@@ -7,6 +7,19 @@ describe ChinChanPu do
 			instruction_element = instruction_set.cutter("A")
 			expect(instruction_element).to eq(["A"])
 		end
+		it 'Returns A Y as single elements' do 
+			instruction_set = ChinChanPu.new
+			instruction_element = instruction_set.cutter("A Y")
+			expect(instruction_element).to eq(["A","Y"])
+		end
 		
+	end
+	context 'Given a set of instructions, sums the points of the figure used' do 
+		it 'returns 1 if my figure used is rock (A)' do 
+			instruction_set = ChinChanPu.new
+			instruction_element = instruction_set.figure?(["A","X"])
+			expect(instruction_element).to eq(1)
+		end
+
 	end
 end
